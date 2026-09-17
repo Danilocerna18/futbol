8f680ca7d0c9ae9c314524064d4fdf538fc3e9c9
MiguelCuevasss <miguecuvas@gmail.com>
2026-09-16 21:07:41 -0600

    Grafo de Mexico vs Arabia Saudita

---
+++ MexVsKsa.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import networkx as nx
+
+datos = pd.read_csv("pases_mexico.csv")
+
+datos = datos[
+    [
+        "match_id",
+        "fecha",
+        "oponente",
+        "jugador_nombre",
+        "receptor_nombre",
+        "resultado"
+    ]
+]
+
+datos = datos[datos["oponente"] == "Saudi Arabia"]
+datos = datos[datos["resultado"] == "Complete"]
+
+# eliminaremos los pases que no tengan jugador o receptor
+datos = datos.dropna(
+    subset=["jugador_nombre", "receptor_nombre"]
+)
+
+#eliminamos los prints porque comprobamos que esta bien
+# Creamos un grafo dirigido
+grafo = nx.DiGraph()
+
+# Agregamos los pases al grafo
+for _, fila in datos.iterrows():
+    jugador = fila["jugador_nombre"]
+    receptor = fila["receptor_nombre"]
+
+    if grafo.has_edge(jugador, receptor):
+        grafo[jugador][receptor]["peso"] += 1
+    else:
+        grafo.add_edge(jugador, receptor, peso=1)
+
+print("Jugadores:", grafo.number_of_nodes())
+print("Conexiones:", grafo.number_of_edges())
+
+import matplotlib.pyplot as plt
+
+# Dibujamos el grafo
+plt.figure(figsize=(8, 10))
+
+posiciones = nx.spring_layout(grafo, seed=42, k=3)
+
+nx.draw(
+    grafo,
+    posiciones,
+    with_labels=True,
+    node_size=1200,
+    node_color="lightgreen",
+    arrows=True,
+    arrowsize=15,
+    width=[grafo[u][v]["peso"] / 2 for u, v in grafo.edges()],
+    font_size=8
+)
+
+plt.title("México vs Saudi Arabia - Pases")
+plt.show()
